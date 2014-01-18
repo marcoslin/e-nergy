47a4f7972b41e54a7e9de9748b46aab750b50234
Marcos Lin <linm.linmair@ebacimail.com>
2014-01-18 14:31:44 +0100

    Added script to parse and post station data to the server

---
+++ data/parse_data.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+
+import sys
+import json
+import httplib, urllib
+
+http_hd = {
+    "Authorization": "Basic YWRtaW46YWRtaW4=",
+    "x-baasbox-appcode": "1234567890",
+    "Content-Type": "application/json"
+}
+
+
+fh = open("charging-station_en_0.csv", "r")
+
+
+number_rows = {"osm-id","version","latitudine","longitudine", "elapsed-time"}
+headers = []
+first_line = True
+
+for line in fh:
+    rows = line.split("\t")
+    
+    if first_line:
+        for row in rows:
+            headers.append(row.strip())
+        first_line = False
+        continue
+    
+    entry = {}
+    row_count = 0
+    for header in headers:
+        entry_value = rows[row_count].strip()
+        try:
+            entry_value = entry_value.encode('ascii', 'ignore')
+        except:
+            print "### Error for %s with value %s" % (header, entry_value)
+            entry_value = ""
+        
+        #print "header: %s; row: %s" % (header, entry_value)
+        if header in number_rows:
+            if entry_value == "":
+                entry[header] = None
+            else:
+                entry[header] = float(entry_value)
+        else:
+            entry[header] = entry_value
+        row_count += 1
+        
+    conn = httplib.HTTPConnection("localhost", 9000)
+    conn.request("POST", "/document/station", json.dumps(entry), http_hd)
+    #conn.request("GET", "/document/station")
+    print "Response: %s" % conn.getresponse()
+    
